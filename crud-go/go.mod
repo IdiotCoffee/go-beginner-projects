5f953496572cf6a33fd155f279f9481191ee9c43
IdiotCoffee <ishaanmavinkurve16@gmail.com>
2026-04-17 19:18:59 +0530

    made a basic http server, and functions for create, get by ID, get all, delete. Update function remaining. Created flowchart for readme.md.

---
+++ crud-go/go.mod
@@ -0,0 +1,5 @@
+module crud-application
+
+go 1.26.1
+
+require github.com/gorilla/mux v1.8.1 // indirect
---
+++ crud-go/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	// here, I tell the client - hey, the next return is a JSON.
+	w.Header().Set("Content-Type", "application/json")
+	// takes the movies variable, converts it into a JSON using encoder, and writes it to w.
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		// if you find the matching ID:
+		if item.ID == params["id"] {
+			// all the movies before appended to all the movies after (after part is actually unfurled!)
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range movies {
+		// if you find the matching ID:
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(&item)
+			break
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	err := json.NewDecoder(r.Body).Decode(&movie) // decode the json in the payload using Decoder
+	if err != nil {
+		fmt.Errorf("Movie Payload NOT correct, please check the input")
+	}
+	movie.ID = (string(rand.Intn(100000000)))
+	movies = append(movies, movie)
+	json.NewEncoder(w)
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbn: "43822", Title: "Inception", Director: &Director{Firstname: "Christopher", Lastname: "Nolan"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "56743", Title: "Whiplash", Director: &Director{Firstname: "Damien", Lastname: "Chazelle"}})
+	movies = append(movies, Movie{ID: "3", Isbn: "29753", Title: "Dead Poet's Society", Director: &Director{Firstname: "Peter", Lastname: "Weir"}})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movie/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movie", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movie/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Println("Starting server on port 8080...")
+	log.Fatal(http.ListenAndServe(":8000", r))
+
+}
